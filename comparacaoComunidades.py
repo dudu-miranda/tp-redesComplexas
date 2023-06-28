f160152cc07e8e51e84072fd0a46a34adaebfc94
dudu-miranda <eduardokira08@gmail.com>
2023-06-27 21:02:06 -0300

    Algoritmo novo para comparacao de comunidades

--- comparacaoComunidades.py
+++ comparacaoComunidades.py
@@ -5,8 +5,23 @@
 from sklearn.metrics.cluster import adjusted_rand_score
 
 def community_similarity(l1, l2):
-    similarity = adjusted_rand_score(l1, l2)
-    return similarity
+    totalElementos = 0
+    similaridade = 0
+    for lista1 in l1:
+        taml1 = len(lista1)
+        totalElementos += taml1
+
+        setl1 = set(lista1)
+        maiorSemelhanca = 0
+        for lista2 in l2:
+            setl2 = set(lista2)
+            common = setl1.intersection(setl2)
+            if len(common) > maiorSemelhanca:
+                maiorSemelhanca = len(common)
+
+        similaridade += maiorSemelhanca
+
+    return similaridade/totalElementos
 
 # Exemplo de uso
 l1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
